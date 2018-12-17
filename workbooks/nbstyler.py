5464acd53f7e26e227917015dc2dc0987dc522af
Deyan Nikolov <deyan.nikolov@gmail.com>
2018-12-17 14:11:33 +0200

    Add a basic styler to centralize common properties

---
+++ workbooks/nbstyler.py
@@ -0,0 +1,24 @@
+STYLER = dict(
+    fonts = dict(
+        default = dict(
+            family = 'Sans Serif',
+            size = 12,
+        ),
+        titlefont = dict(
+            family = 'Sans Serif',
+            size = 14,
+        ),
+    ),
+    colors = dict(
+        bg1 = '#FFFBF3',
+        bg2 = '#E8E3D1',
+        bg3 = '#E8E3D1',
+        fg1 = '#7F7D73',
+        fg2 = '#403E39',
+        acc1 = '#7D82B2',
+        acc2 = '#FFE999',
+        acc3 = '#995959',
+        acc4 = '#CCA3CC',
+        acc5 = '#CCFFF7',
+    ),
+)
