8f72bffa7c2133e6c7c151e8295066da1011a696
Deyan Nikolov <deyan.nikolov@gmail.com>
2018-12-18 14:18:43 +0200

    Add programming languages demand chart

--- workbooks/nbstyler.py
+++ workbooks/nbstyler.py
@@ -1,24 +1,27 @@
-STYLER = dict(
+STYLE = dict(
     fonts = dict(
         default = dict(
             family = 'Sans Serif',
-            size = 12,
-        ),
+            size = 12,),
         titlefont = dict(
             family = 'Sans Serif',
-            size = 14,
-        ),
-    ),
+            size = 16,),),
     colors = dict(
-        bg1 = '#FFFBF3',
-        bg2 = '#E8E3D1',
-        bg3 = '#E8E3D1',
-        fg1 = '#7F7D73',
-        fg2 = '#403E39',
-        acc1 = '#7D82B2',
-        acc2 = '#FFE999',
-        acc3 = '#995959',
-        acc4 = '#CCA3CC',
-        acc5 = '#CCFFF7',
+        solid = dict(
+            bg1 = '#FFFBF3',
+            bg2 = '#E8E3D1',
+            bg3 = '#E8E3D1',
+            fg1 = '#7F7D73',
+            fg2 = '#403E39',
+            acc1 = 'rgba(125,130,178,1)',
+            acc2 = 'rgba(255,233,153,1)',
+            acc3 = '#995959',
+            acc4 = '#CCA3CC',
+            acc5 = '#CCFFF7',
+        ),
+        opaque = dict(
+            acc1 = 'rgba(125,130,178,0.5)',
+            acc2 = 'rgba(255,233,153,0.5)',
+        )
     ),
 )
